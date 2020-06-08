7b1136f2f270c8cd40e4e2cca380bfca18a898bb
Natalya <natalya.pa2016@gmail.com>
2020-06-08 23:00:17 +0300

    Add backend directory

---
+++ flask-backend/__pycache__/main.py
@@ -0,0 +1,45 @@
+from bson import ObjectId
+
+import gift
+from flask import Flask, jsonify, render_template, Response, json
+import pymongo
+import ssl
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+@app.route('/')
+@app.route('/index')
+def main_page():
+    return "Привет, мир!"
+
+def connect_to_database():
+    client = pymongo.MongoClient(
+        "mongodb+srv://vdno:kurluk@cluster0-lesec.gcp.mongodb.net/test?retryWrites=true&w=majority")
+    db = client.database
+    collection = db.gift
+    return collection
+
+@app.route('/catalog', methods=['GET'])
+def get_all():
+    collection = connect_to_database()
+    my_cursor = collection.find({})
+
+    result = []
+    for item in my_cursor:
+        item["_id"] = str(item["_id"])
+        result.append(item)
+    return jsonify({'result': result})
+    #return result
+    return render_template("index.html",
+                           result=result)
+
+def get_by_id(id):
+    collection = connect_to_database()
+    my_cursor = collection.find({"_id" : ObjectId(id)})
+    result = []
+    for item in my_cursor:
+        result.append(item)
+    return jsonify({'result': result})
+
+app.run()
---
+++ flask-backend/gift.py
@@ -0,0 +1,14 @@
+class gift:
+    def __init__(self):
+        self.description = ""
+        self.image = ''
+
+    def add_item(self):
+        return "kurluk"
+    def delete_item(self):
+        pass
+    def update_item(self):
+        pass
+    def get_item(self):
+        pass
+    
