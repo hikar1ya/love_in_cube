200284ca0b8f671b9b680107fde97e5d3d6eaf17
Victoria Koposova <koposova.victoria17@mail.ru>
2020-06-10 20:39:14 +0300

    Try to fetch data

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -3,8 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      list: [],
+    };
+  }
+
+  componentDidMount(){
+    fetch("http://127.0.0.1:5000/catalog")
+    .then((response) => response.json())
+    .then((json) => {
+      this.setState({
+        loading: false,
+        list: json.results,
+      });
+    });
+  }
+
   render() {
-  return <h1>Alo kurluk: {window.result.json()}</h1>;
+  return <div>Alo kurluk: {this.state.loading ? "Loading..." : "Everything is ok!"}</div>;
   }
 }
 
