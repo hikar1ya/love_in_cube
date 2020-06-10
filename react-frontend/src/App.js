669c018a3b8dd9520bbf3bdf4ac4b10aeaa73a41
Victoria Koposova <koposova.victoria17@mail.ru>
2020-06-10 21:03:49 +0300

    Recreate frontend files

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,29 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true,
-      list: [],
-    };
-  }
-
-  componentDidMount(){
-    fetch("http://127.0.0.1:5000/catalog")
-    .then((response) => response.json())
-    .then((json) => {
-      this.setState({
-        loading: false,
-        list: json.results,
-      });
-    });
-  }
-
-  render() {
-  return <div>Alo kurluk: {this.state.loading ? "Loading..." : "Everything is ok!"}</div>;
-  }
+function App() {
+  return <div>Hello, world!</div>
 }
 
 export default App;
