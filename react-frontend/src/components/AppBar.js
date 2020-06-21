e4bf2a9807b2e71d0eedf767d63f455b72c18aad
Victoria Koposova <koposova.victoria17@mail.ru>
2020-06-21 12:27:29 +0300

    Change App Bar

--- react-frontend/src/components/AppBar.js
+++ react-frontend/src/components/AppBar.js
@@ -1,51 +1,30 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom'
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  toolbar: {
-    minHeight: 128,
-    alignItems: 'flex-start',
-    paddingTop: theme.spacing(1),
-    paddingBottom: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-    alignSelf: 'flex-end',
-  },
-}));
+import './List.css'
 
 export default function ProminentAppBar() {
-  const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar className={classes.toolbar}>
-          <Typography className={classes.title} variant="h5" noWrap>
-            Love In Cube
-          </Typography>
-          <Button color="black">
-            <Link to="/">Подарки</Link>
-          </Button>
-          <Button color="black">
-            <Link to="/about">О нас</Link>
-          </Button>
-          <Button color="black">
-            <Link to="/how_to_order">Как заказать</Link>
-          </Button>
-          <Button color="black">
-            <Link to="/basket">Корзина</Link>
-          </Button>
-        </Toolbar>
-      </AppBar>
+    <div style={{ height: '125px', backgroundColor: '#fff5d1', verticalAlign: 'center' }}>
+      <div style={{ textAlign: 'center' }}>
+        <Typography variant='h4' style={{ fontFamily: 'cursive' }}>Love In Cube</Typography>
+      </div>
+      <div style={{ marginLeft: '135px' }}>
+        <Button color="black">
+          <Link to="/">Подарки</Link>
+        </Button>
+        <Link to="/about">
+          <Button color="black">О нас</Button>
+        </Link>
+        <Button color="black">
+          <Link to="/how_to_order">Как заказать</Link>
+        </Button>
+        <Button color="black">
+          <Link to="/basket">Корзина</Link>
+        </Button>
+      </div>
     </div>
   );
 }
